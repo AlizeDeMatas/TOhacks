27f588f0f4a7d25a400d36299eab5cc8cfcfbffd
flowerbrook <48869154+flowerbrook@users.noreply.github.com>
2020-05-03 10:46:48 -0400

    Added a delete functionality

--- to_do.py
+++ to_do.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 inputTxt = ""
+delOutTxt = ""
 
 class to_do:
 
@@ -21,15 +22,15 @@ class to_do:
 		myLabel.pack(padx = 5,
 					 pady = 5)
 
-		#Text entry
+		#Add Text entry
 		self.entryTxt = StringVar()
-		myEntry = Entry(window,
+		self.myEntry = Entry(window,
 						textvariable = self.entryTxt,
 						width = 50)
-		myEntry.pack(padx = 5,
-					 pady = 5)
+		self.myEntry.pack(padx = 5,
+					 	  pady = 5)
 
-		#Button
+		#Add Button
 		myButton = Button(text = "Enter",
 						  width = 5,
 						  height = 1,
@@ -40,6 +41,33 @@ class to_do:
 					  pady = 5)
 		myButton.bind("<Button-1>", self.clickHandle)
 
+		#Delete Text entry
+		self.delEntryTxt = StringVar()
+		self.delEntry = Entry(window,
+							  textvariable = self.delEntryTxt,
+							  width = 50)
+		self.delEntry.pack(padx = 5,
+					 	   pady = 5)
+
+		#Delete Button
+		delButton = Button(text = "Delete",
+						   width = 5,
+						   height = 1,
+						   bg = "white",
+						   fg = "black",
+						   command = self.delHandle)
+		delButton.pack(padx = 5,
+					   pady = 5)
+		delButton.bind("<Button-2>", self.delHandle)
+
+		#Output text label
+		outputLabel = Label(window,
+					   		text = "Not Done",
+					   		bg = "#D7EAF7",
+					   		font= "System 10 bold")
+		outputLabel.pack(padx = 5,
+						 pady = 5)
+
 		#Output text
 		self.outputTxt = StringVar()
 		output = Label(window,
@@ -48,7 +76,23 @@ class to_do:
 		output.pack(padx = 5,
 					pady = 5)
 
-		#Main the program
+		#Done output text label
+		delLabel = Label(window,
+					   	 text = "Done",
+					   	 bg = "#D7EAF7",
+					   	 font= "System 10 bold")
+		delLabel.pack(padx = 5,
+					  pady = 5)
+
+		#Done output text
+		self.delTxt = StringVar()
+		delOut = Label(window,
+					   textvariable = self.delTxt,
+					   bg = "#D7EAF7")
+		delOut.pack(padx = 5,
+					pady = 5)
+
+		#Loop the program
 		window.mainloop();
 
 	#Button reaction event
@@ -56,9 +100,16 @@ class to_do:
 	def clickHandle(self,event):
 		global inputTxt 
 		inputTxt = inputTxt + "\n" + self.entryTxt.get() #Get input
-		outTxt = self.outputTxt.set(format(inputTxt))
+		outTxt = self.outputTxt.set(format(inputTxt)) #Copy input into output
+
+		self.myEntry.delete(0, END) #Delete input
 		return outTxt
 
+	def delHandle(self,event):
+		global delOutTxt
+		delOutTxt = delOutTxt + "\n" + self.delEntryTxt.get() #Get input
+		outTxt = self.delTxt.set(format(delOutTxt)) #Copy input into output
+		return outTxt
 
 
 to_do()
